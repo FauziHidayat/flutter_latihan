0a3932335e7bc01ab085e0c80568f71ec3f7c654
FauziHidayat <hidayat.fauzi52@gmail.com>
2022-02-11 22:29:17 +0700

    discover page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_latihan/latihan/app_bar_lat.dart';
 import 'package:flutter_latihan/latihan/card_lat.dart';
 import 'package:flutter_latihan/latihan/container_lat.dart';
+import 'package:flutter_latihan/latihan/discover.dart';
 import 'package:flutter_latihan/latihan/login.dart';
 import 'package:flutter_latihan/latihan/tea_page.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TeaPage (),
+      home: DiscoverPage(),
     );
   }
 }
